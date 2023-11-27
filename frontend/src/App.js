7228cdf15fdff3c1c43f0e0cfabff817ccdce07c
vigneshnm64 <42004258+vigneshnm64@users.noreply.github.com>
2023-11-27 13:13:58 +0530

    vigesh route modified
    route modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import {Routes,Route} from 'react-router-dom'
+import Home from './Home'
+import About from './About'
 function App() {
   return (
-    <div>App</div>
+    <div>
+    <Routes>
+    <Route path='/' element={<Home/>}></Route>
+    <Route path='/about' element={<About/>}></Route>
+    </Routes>
+    </div>
   )
 }
 
